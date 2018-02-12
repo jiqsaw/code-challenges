51b5b42ebac43234a367515689a93115bd0c820d
burakucakan <burakucakan@gmail.com>
2018-02-12 16:41:28 +0000

    js folder deleted

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// import './change-calculation';
-// import './collect-cards';
-// import './cyclic-rotation';
-// import './frog-jmp';
-// import './perm-check';
-// import './perm-missing-elements';
-// import './tape_equilibrium';
-//# sourceMappingURL=index.js.map
