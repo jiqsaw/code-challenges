54f1fc07c6d2014ab1430a5fb8b3ec03222e927d
Burak UCAKAN <burakucakan@192.168.1.109>
2018-02-18 20:19:25 +0000

    max product of three

---
+++ src/triangle.ts
@@ -0,0 +1,30 @@
+/*
+
+    https://app.codility.com/programmers/lessons/6-sorting/triangle/
+    
+    Determine whether a triangle can be built from a given set of edges.
+
+*/
+
+console.log('// TRIANGLE //');
+
+function triangle(A: number[]): number {
+
+    
+
+    return null;
+
+}
+
+function triangleTest(A: number[]) {
+    console.log('\n(', A, ')\n');
+    console.log('\n=>', triangle(A));
+    console.log('\n--------------------------------------\n');
+}
+
+
+triangleTest([10, 2, 5, 1, 8, 20]);
+// 1
+
+triangleTest([10, 50, 5, 1]);
+// 0
