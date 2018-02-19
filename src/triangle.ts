c92ecc2eddbc01848341805d13d0c8384e59bf8e
burakucakan <burakucakan@gmail.com>
2018-02-19 15:43:16 +0000

    triangle optimised

--- src/triangle.ts
+++ src/triangle.ts
@@ -8,88 +8,24 @@
 
 console.log('// TRIANGLE //');
 
-// function triangle(A: number[]): number {
-
-//     let i = 0;
-//     let len = A.length;
-
-//     while (i < len) {
-
-//         let p = A[i];
-
-//         console.log('\n p: ', p)
-
-//         let j = 0;
-//         while (j < len - 1) {
-
-//             if (i !== j) {
-
-//                 let q = A[j];
-//                 console.log('  q: ', q)
-
-//                 if (A.filter((r, k) => k !== j && k !== i &&
-//                     (p + q > r) && (p + r > q) && (q + r > p)
-//                 ).length > 0) {
-
-//                     return 1;
-//                 }
-//                 // let k = j + 1;
-
-//                 // while (k < len) {
-
-//                 //     let r = A[k];
-//                 //     console.log('    r: ', r)
-
-//                 //     if ((k !== j) && (k !== i)) {
-
-//                 //         if ((p + q > r) && (p + r > q) && (q + r > p)) {
-//                 //             console.log(`p: ${p}  -  q: ${q}  -  r: ${r} \n`)
-//                 //             return 1
-//                 //         }
-
-//                 //     }
-//                 //     k++;
-
-//                 // }
-
-//             }
-
-//             j++;
-//         }
-
-//         i++;
-//     }
-
-//     return 0;
-
-// }
-
-
 function triangle(A: number[]): number {
 
     let i = 0;
     let len = A.length;
 
+    A = A.sort((a, b) => b - a);
+
     while (i < len) {
 
         let p = A[i];
 
-        console.log('\n p: ', p)
+        let j = i + 1;
 
-        let j = 0;
-        while (j < len - 1) {
+        let q = A[i + 1];
+        let r = A[i + 2];
 
-            if (i !== j) {
-                let q = A[j];
-
-                // let r = 
-                if (q < ((p + q) + q)) {
-                    return 1;
-                }
-
-            }
-
-            j++;
+        if ((p + q > r) && (p + r > q) && (q + r > p)) {
+            return 1;
         }
 
         i++;
@@ -115,41 +51,5 @@ triangleTest([10, 50, 5, 1]);
 triangleTest([5, 3, 3]);
 // 1
 
-/*
-
-
-
-x + y > z,
-y + z > x,
-z + x > y.
-
-z < x + y
-
-
-
-?? 10 + 2 > X
-?? 10 + x > 2
-
-formula;
-
-A[P] + A[Q] > A[R],
-A[Q] + A[R] > A[P],
-A[R] + A[P] > A[Q].
-
-10, 5, 8
-10 +  5 = 15 >  8
- 5 +  8 = 13 > 10
- 8 + 10 = 18 >  5
-
-3, 5, 3
-3 + 5 > 3
-5 + 3 > 3
-3 + 3 > 5
-
-p = p
-q = r + int(p / r) + 1
-r = p / 2
-
-
-
- */
+triangleTest([-2, -2, -2]);
+// 0
