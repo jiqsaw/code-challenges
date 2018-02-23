6646840a1a63fa99a5f8cd4e1a6f67d1e1bcc72a
burakucakan <burakucakan@gmail.com>
2018-02-23 17:28:18 +0000

    stone wall

--- js/stone-wall.js
+++ js/stone-wall.js
@@ -13,29 +13,45 @@ function solution(A) {
     if (A.length < 1) return 0;
     if (A.length === 1) return 1;
 
-    let i = 1;
-    let total = Number(A[i] === A[i + 1]);
-    while (i < A.length - 2) {
-        total += Number(A[i] !== A[i + 1]);
+    let i = 0;
+    let len = A.length;
+    let min = A[0];
+    let gap = 0;
+    let blocks = 1;
+    while (i < len) {
+        if (A[i] < min) {
+            min = A[i];
+            blocks++;
+        } else if (A[i] > min) {
+            if (gap === 0) {
+                gap = i;
+            }
+        }
         i++;
-    }
-    total += Number(A[A.length - 2] === A[A.length]);
-    return total;
+        
+        if ((i === len) && (gap > 0) && (gap <= len)) {
+            i = gap;
+            gap = 0;
+            min = A[i];
+        }
+
+        console.log('blocks', blocks, 'gap', gap, '=', A[gap]);
+    }    
+    console.log('blocks', blocks)
+    return blocks;
 }
 
-
-test([1]);
+// test([1]);
 // 1
 
-test([8, 8, 5, 7, 9, 8, 7, 4, 8]);
+// test([8, 8, 5, 7, 9, 8, 7, 4, 8]);
 // 7
 
-test([3, 2, 1]);
+// test([3, 2, 1]);
 // 3
 
-test([1, 1, 1]);
+test([2, 3, 2, 1]);
 // 3
-
 
 
 
