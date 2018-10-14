058fba640dfa80d2927ad0d8fe1f474a767f72e8
Burak <burakucakan@gmail.com>
2018-10-14 01:07:22 +0100

    stonewall correct

--- js/stone-wall.js
+++ js/stone-wall.js
@@ -8,49 +8,81 @@
 
 console.log('STONE WALL');
 
-function solution(A) {
 
-    if (A.length < 1) return 0;
-    if (A.length === 1) return 1;
+// remove next same items
+function solution(H) {
+
+    if (H.length < 1) return 0;
+    if (H.length === 1) return 1;
 
     let i = 0;
-    let len = A.length;
-    let min = A[0];
-    let gap = 0;
-    let blocks = 1;
-    while (i < len) {
-        if (A[i] < min) {
-            min = A[i];
-            blocks++;
-        } else if (A[i] > min) {
-            if (gap === 0) {
-                gap = i;
+    let B = [];
+
+    while (i < H.length) {
+
+        nextIndex = H.indexOf(H[i], i + 1);
+
+        if (nextIndex !== -1) {
+            
+            B = H.slice(i, nextIndex);
+
+            if (Math.min(...B) === H[i]) {
+                H.splice(nextIndex, 1);
+                i--;
             }
-        }
+
+        }        
+
         i++;
-        
-        if ((i === len) && (gap > 0) && (gap <= len)) {
-            i = gap;
-            gap = 0;
-            min = A[i];
+    }
+
+    return H.length;
+}
+
+
+// count unrequired previous items
+function solution2(H) {
+
+    if (H.length < 1) return 0;
+    if (H.length === 1) return 1;
+
+    let i = 1;
+    let len = H.length;
+    let sameCount = 0;
+
+    let B = [];
+
+    while (i < len) {
+
+        if (H.slice(0, i).lastIndexOf(H[i]) !== -1) {
+            
+            B = H.slice(H.slice(0, i).lastIndexOf(H[i]), i);
+
+            if (Math.min(...B) === H[i]) {
+                sameCount++;
+            }
+
         }
 
-        console.log('blocks', blocks, 'gap', gap, '=', A[gap]);
-    }    
-    console.log('blocks', blocks)
-    return blocks;
+        i++;
+    }
+
+    return H.length - sameCount;
 }
+
+// test([1, 1, 1]);
+// 1
 
 // test([1]);
 // 1
 
-// test([8, 8, 5, 7, 9, 8, 7, 4, 8]);
+test([8, 8, 5, 7, 9, 8, 7, 4, 8]);
 // 7
 
 // test([3, 2, 1]);
 // 3
 
-test([2, 3, 2, 1]);
+// test([2, 3, 2, 1]);
 // 3
 
 
